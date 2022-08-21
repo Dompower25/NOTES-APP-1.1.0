bbed8fbf7e62b9545c3c10069513677a0649d75b
Evgeniy Domanovich <dompro1994.ed@gmail.com>
2022-08-21 19:16:56 +0300

    drawing modal and notes

--- src/App.js
+++ src/App.js
@@ -17,10 +17,8 @@ import { NotesContext } from "./hooks/useNote";
 function App() {
   const [user] = useContext(UserContext);
   const [notes] = useContext(NotesContext);
-
   const [searchTag, setSearchTag] = useState("");
   const filteredNotes = useSearch(notes, searchTag); //хук поиска тегов
-
   const [showAuth, setShowAuth] = useState(true);
   const [showRegistration, setShowRegistration] = useState(false);
 
@@ -31,77 +29,79 @@ function App() {
 
   return (
     <div className="App">
-      <CSSTransition
-        in={showModal}
-        timeout={500}
-        classNames="modal"
-        unmountOnExit
-        onEnter={() => setShowModal(true)}
-        onExited={() => setShowModal(false)}
-      >
-        <Modal className="modal">
-          <div className={st.login__form}>
-            {showAuth && (
-              <div>
-                <span className={st.log}>LOGIN</span>
-                <LogInForm />
-                <button
-                  className="not_autorisation"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setShowRegistration(true);
-                  }}
-                >
-                  Haven't you got an account yet?
-                </button>
-              </div>
-            )}
-            <CSSTransition
-              in={showRegistration}
-              timeout={250}
-              classNames="register__form"
-              unmountOnExit
-              onEnter={() => setShowAuth(false)}
-              onExited={() => setShowAuth(true)}
-            >
-              <div className="register__form">
-                <span className={st.log}>REGISTRATION</span>
-                <SingUpForm setShowRegistration={setShowRegistration} />
-              </div>
-            </CSSTransition>
-          </div>
-        </Modal>
-      </CSSTransition>
-
-      <div className="notes__app">
-        <HeaderForm />
-        <h1>NOTES APP</h1>
-        <NotesForm />
-        <hr></hr>
-        {notes ? (
-          <SearchInput value={searchTag} onChange={setSearchTag} />
-        ) : (
-          <Loader />
-        )}
-        <TransitionGroup className="list">
-          {filteredNotes.map(({ text, id, tags, creationDate }) => (
-            <CSSTransition
-              key={creationDate}
-              timeout={500}
-              classNames="noteList"
-            >
-              <NoteItem
-                className="noteList"
-                note={searchTag}
-                bodyNote={text}
-                id={id}
-                tag={tags}
-                timeCreate={creationDate}
-              />
-            </CSSTransition>
-          ))}
-        </TransitionGroup>
-      </div>
+      {showModal ? (
+        <CSSTransition
+          in={showModal}
+          timeout={500}
+          classNames="modal"
+          unmountOnExit
+          onEnter={() => setShowModal(true)}
+          onExited={() => setShowModal(false)}
+        >
+          <Modal className="modal">
+            <div className={st.login__form}>
+              {showAuth && (
+                <div>
+                  <span className={st.log}>LOGIN</span>
+                  <LogInForm />
+                  <button
+                    className="not_autorisation"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setShowRegistration(true);
+                    }}
+                  >
+                    Haven't you got an account yet?
+                  </button>
+                </div>
+              )}
+              <CSSTransition
+                in={showRegistration}
+                timeout={250}
+                classNames="register__form"
+                unmountOnExit
+                onEnter={() => setShowAuth(false)}
+                onExited={() => setShowAuth(true)}
+              >
+                <div className="register__form">
+                  <span className={st.log}>REGISTRATION</span>
+                  <SingUpForm setShowRegistration={setShowRegistration} />
+                </div>
+              </CSSTransition>
+            </div>
+          </Modal>
+        </CSSTransition>
+      ) : (
+        <div className="notes__app">
+          <HeaderForm />
+          <h1>NOTES APP</h1>
+          <NotesForm />
+          <hr></hr>
+          {notes ? (
+            <SearchInput value={searchTag} onChange={setSearchTag} />
+          ) : (
+            <Loader />
+          )}
+          <TransitionGroup className="list">
+            {filteredNotes.map(({ text, id, tags, creationDate }) => (
+              <CSSTransition
+                key={creationDate}
+                timeout={500}
+                classNames="noteList"
+              >
+                <NoteItem
+                  className="noteList"
+                  note={searchTag}
+                  bodyNote={text}
+                  id={id}
+                  tag={tags}
+                  timeCreate={creationDate}
+                />
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </div>
+      )}
     </div>
   );
 }
